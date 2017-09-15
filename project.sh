06bacbcac0bfe845afc5aeebc9ec22a12a50a123
bryangunn <30510064+bryangunn@users.noreply.github.com>
2017-09-15 11:17:34 -0400

    Delete project.sh

--- project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo
-echo '********************************************'
-echo '*                                          *'
-echo '* This is some project code for a cool fun *'
-echo '*                                          *'
-echo '********************************************'
