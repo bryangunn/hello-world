5392b067732e83e955855982932e374dbe7fa759
Bryan Gunn <bryangunn@Bryans-MacBook-Pro-3.local>
2017-09-15 14:54:21 -0400

    just triggering travis to do a build

--- project.sh
+++ project.sh
@@ -3,6 +3,6 @@
 echo
 echo '********************************************'
 echo '*                                          *'
-echo '* This is some project code for a cool fun *'
+echo '* This is some project code for a tool fun *'
 echo '*                                          *'
 echo '********************************************'
